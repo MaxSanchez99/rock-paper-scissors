0eb05c7c1b2fee49806702d3e66fb05b5b2233bb
Max Sanchez <145044997+MaxSanchez99@users.noreply.github.com>
2025-09-09 20:13:14 -0500

    Created the UI for the game
    Made buttons for the user to make their choice.
    Both the user's and computer's score are displayed
    along with the results of each game. Game
    ends after someone reaches 5 points.

--- script.js
+++ script.js
@@ -21,56 +21,62 @@ function getHumanChoice() {
 
 }
 
-function playGame() {
-    // Initialize score count 
-    let humanScore = 0
-    let computerScore = 0
+// Initialize score count 
+let humanScore = 0
+let computerScore = 0
 
-    // Declare function to play one round
-    const playRound = (humanChoice, computerChoice) => {
-        let newHumanChoice = humanChoice.toLowerCase();
-        let message = "";
+let humanScoreText = document.querySelector('#human-score');
+let computerScoreText = document.querySelector('#computer-score');
+let messageDiv = document.querySelector('#message');
 
-        if (newHumanChoice === computerChoice) {
-            message = "You tied!"
-        }
+// Declare function to play one round
+const playRound = (humanChoice, computerChoice) => {
+    let newHumanChoice = humanChoice.toLowerCase();
+    let message = "";
 
-        else if (
-            (newHumanChoice === 'rock' && computerChoice === 'scissors') ||
-            (newHumanChoice === 'paper' && computerChoice === 'rock') ||
-            (newHumanChoice === 'scissors' && computerChoice === 'paper')
-        ) {
-            message = `You win!, ${newHumanChoice} beats ${computerChoice}.`
-            humanScore++;
-        }
-        else {
-            message = `You lose!, ${computerChoice} beats ${newHumanChoice}.`
-            computerScore++;
-        }
-        return message;
+    if (newHumanChoice === computerChoice) {
+        message = "You tied!"
     }
 
-    // Run 5 rounds of the game
-    let i = 0
-    while (i < 5) {
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-        console.log(playRound(humanSelection, computerSelection));
-        console.log("Human Score: " + humanScore);
-        console.log("Computer Score: " + computerScore);
-        i++;
-    }
+    else if (
+        (newHumanChoice === 'rock' && computerChoice === 'scissors') ||
+        (newHumanChoice === 'paper' && computerChoice === 'rock') ||
+        (newHumanChoice === 'scissors' && computerChoice === 'paper')
+    ) {
 
-    // Declare the winner
-    if (humanScore > computerScore) {
-        alert("You win!");
-    }
-    else if (humanScore < computerScore) {
-        alert("Computer wins!");
+        message = `You win! ${newHumanChoice} beats ${computerChoice}`;
+        humanScore++;
+        humanScoreText.textContent = humanScore.toString();
     }
     else {
-        alert("Game ends in a tie!");
+        message = `You lose! ${computerChoice} beats ${newHumanChoice}`;
+        computerScore++;
+        computerScoreText.textContent = computerScore.toString();
     }
+
+    if (humanScore === 5) {
+        announceWinner('user');
+    } else if (computerScore === 5) {
+        announceWinner('computer');
+    }
+
+    messageDiv.textContent = message;
+    console.log(message);
 }
 
-playGame();
+let choices = document.querySelector("#choices")
+
+choices.addEventListener('click', (event) => {
+    let computerChoice = getComputerChoice();
+    playRound(event.target.id, computerChoice);
+});
+
+function announceWinner(winner) {
+    alert("Winner is: " + winner);
+
+    humanScoreText.textContent = '0';
+    humanScore = 0;
+
+    computerScoreText.textContent = '0';
+    computerScore = 0;
+}
